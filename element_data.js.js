a0f6ed4ec194073ba58ee0ecb74d3810bff23f5b
okay7290 <okay7290@gmail.com>
2025-01-17 00:23:12 +0700

    feat: add 100+ new combination

---
+++ element_data.js.js
@@ -0,0 +1,990 @@
+
+    // Element Combiner Data
+    const elementData = {
+  "elements": [
+    {
+      "text": "Fire",
+      "emoji": "\ud83d\udd25",
+      "discovered": false
+    },
+    {
+      "text": "Water",
+      "emoji": "\ud83d\udca7",
+      "discovered": false
+    },
+    {
+      "text": "Wind",
+      "emoji": "\ud83d\udca8",
+      "discovered": false
+    },
+    {
+      "text": "Earth",
+      "emoji": "\ud83c\udf0e",
+      "discovered": false
+    },
+    {
+      "text": "Windsurfing",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Snot",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Storm",
+      "emoji": "\u26c8\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Beach",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Mud",
+      "emoji": "\ud83d\udca9",
+      "discovered": false
+    },
+    {
+      "text": "Phoenix",
+      "emoji": "\ud83d\udd25",
+      "discovered": false
+    },
+    {
+      "text": "Monocle",
+      "emoji": "\ud83e\uddd0",
+      "discovered": false
+    },
+    {
+      "text": "Sneeze",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Surfboard",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Tsunami",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Tea party",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Glass",
+      "emoji": "\ud83e\udd43",
+      "discovered": false
+    },
+    {
+      "text": "Mud",
+      "emoji": "\ud83d\udca9",
+      "discovered": false
+    },
+    {
+      "text": "Smoke",
+      "emoji": "\ud83d\udca8",
+      "discovered": false
+    },
+    {
+      "text": "Oasis",
+      "emoji": "\ud83c\udfdc\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Snot",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Allergy",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Hurricane",
+      "emoji": "\ud83c\udf00",
+      "discovered": false
+    },
+    {
+      "text": "Surfer",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Phoenix",
+      "emoji": "\ud83d\udd25",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Surf",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Mud",
+      "emoji": "\ud83d\udca9",
+      "discovered": false
+    },
+    {
+      "text": "Cough",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Sand",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Sand",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Tsunami",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Marsh",
+      "emoji": "\ud83c\udf3f",
+      "discovered": false
+    },
+    {
+      "text": "Sand",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Bee",
+      "emoji": "\ud83d\udc1d",
+      "discovered": false
+    },
+    {
+      "text": "Sand",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Oasis",
+      "emoji": "\ud83c\udfdc\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Allergy",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Mirage",
+      "emoji": "\ud83c\udf35",
+      "discovered": false
+    },
+    {
+      "text": "Snot",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Rain",
+      "emoji": "\ud83c\udf27\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Surfer",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Mucus",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Sand",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Cactus",
+      "emoji": "\ud83c\udf35",
+      "discovered": false
+    },
+    {
+      "text": "Ash",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Sauna",
+      "emoji": "\ud83e\uddd6\u200d\u2640\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Honey",
+      "emoji": "\ud83c\udf6f",
+      "discovered": false
+    },
+    {
+      "text": "Volcano",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Surfboard",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Cloud",
+      "emoji": "\u2601\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Tea",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Rain",
+      "emoji": "\ud83c\udf27\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Lotus",
+      "emoji": "\ud83c\udf3a",
+      "discovered": false
+    },
+    {
+      "text": "Leprechaun",
+      "emoji": "\ud83c\udf40",
+      "discovered": false
+    },
+    {
+      "text": "Lava",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Oasis",
+      "emoji": "\ud83c\udfdc\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Buddha",
+      "emoji": "\ud83e\uddd8\u200d\u2642\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Ocean",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Surfboard",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Wave",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Cyclops",
+      "emoji": "\ud83d\udc41\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Surfing",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Lava",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Tsunami",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Tsunami",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Volcano",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Lava",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Snot",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Volcano",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Mad hatter",
+      "emoji": "\ud83c\udfa9",
+      "discovered": false
+    },
+    {
+      "text": "Glass",
+      "emoji": "\ud83e\udd43",
+      "discovered": false
+    },
+    {
+      "text": "Mucus",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Tea",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Beach",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Pollen",
+      "emoji": "\ud83c\udf31",
+      "discovered": false
+    },
+    {
+      "text": "Surfboard",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Surfer",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Tornado",
+      "emoji": "\ud83c\udf2a\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Sandcastle",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Steamboard",
+      "emoji": "\ud83d\udef9",
+      "discovered": false
+    },
+    {
+      "text": "Tsunami",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Snot",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Honey",
+      "emoji": "\ud83c\udf6f",
+      "discovered": false
+    },
+    {
+      "text": "Sand",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Volcano",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Sweat",
+      "emoji": "\ud83d\udca6",
+      "discovered": false
+    },
+    {
+      "text": "Bog",
+      "emoji": "\ud83d\udca9",
+      "discovered": false
+    },
+    {
+      "text": "Snot",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Train",
+      "emoji": "\ud83d\ude82",
+      "discovered": false
+    },
+    {
+      "text": "Wave",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Kaleidoscope",
+      "emoji": "\ud83d\udd2e",
+      "discovered": false
+    },
+    {
+      "text": "Surf",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Sneeze",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Kite",
+      "emoji": "\ud83e\ude81",
+      "discovered": false
+    },
+    {
+      "text": "Tea",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Mad hatter",
+      "emoji": "\ud83c\udfa9",
+      "discovered": false
+    },
+    {
+      "text": "Malaria",
+      "emoji": "\ud83e\udd9f",
+      "discovered": false
+    },
+    {
+      "text": "Snotami",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Snotty",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Crocodile",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Rain",
+      "emoji": "\ud83c\udf27\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Hay fever",
+      "emoji": "\ud83c\udf3e",
+      "discovered": false
+    },
+    {
+      "text": "Allergies",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Tsunami",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Dandelion",
+      "emoji": "\ud83c\udf3c",
+      "discovered": false
+    },
+    {
+      "text": "Electricity",
+      "emoji": "\u26a1\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Surfing",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Mosquito",
+      "emoji": "\ud83e\udd9f",
+      "discovered": false
+    },
+    {
+      "text": "Engine",
+      "emoji": "\ud83d\ude97",
+      "discovered": false
+    },
+    {
+      "text": "Storm",
+      "emoji": "\u26c8\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Eruption",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Volcano",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Bee",
+      "emoji": "\ud83d\udc1d",
+      "discovered": false
+    },
+    {
+      "text": "Earthquake",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Sneeze",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Train",
+      "emoji": "\ud83d\ude82",
+      "discovered": false
+    },
+    {
+      "text": "Rainbow",
+      "emoji": "\ud83c\udf08",
+      "discovered": false
+    },
+    {
+      "text": "Tea party",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Windmill",
+      "emoji": "\ud83c\udf2c\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Volcano",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Steam",
+      "emoji": "\ud83d\udca8",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Plant",
+      "emoji": "\ud83c\udf31",
+      "discovered": false
+    },
+    {
+      "text": "Honey",
+      "emoji": "\ud83c\udf6f",
+      "discovered": false
+    },
+    {
+      "text": "Honeycomb",
+      "emoji": "\ud83c\udf6f",
+      "discovered": false
+    },
+    {
+      "text": "Volcano",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Snotamire",
+      "emoji": "\ud83d\udc43",
+      "discovered": false
+    },
+    {
+      "text": "Dirty mad hatter",
+      "emoji": "\ud83c\udfa9",
+      "discovered": false
+    },
+    {
+      "text": "Sandman",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Palm",
+      "emoji": "\ud83c\udf34",
+      "discovered": false
+    },
+    {
+      "text": "Bee",
+      "emoji": "\ud83d\udc1d",
+      "discovered": false
+    },
+    {
+      "text": "Phoenix",
+      "emoji": "\ud83d\udd25",
+      "discovered": false
+    },
+    {
+      "text": "Sand",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Tsunami",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Ocean",
+      "emoji": "\ud83c\udf0a",
+      "discovered": false
+    },
+    {
+      "text": "Bee",
+      "emoji": "\ud83d\udc1d",
+      "discovered": false
+    },
+    {
+      "text": "Booger",
+      "emoji": "\ud83d\udc43",
+      "discovered": false
+    },
+    {
+      "text": "Tea party",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Beach",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Planet",
+      "emoji": "\ud83e\ude90",
+      "discovered": false
+    },
+    {
+      "text": "Volcano",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Firebird",
+      "emoji": "\ud83d\udc26",
+      "discovered": false
+    },
+    {
+      "text": "Cough syrup",
+      "emoji": "\ud83e\udd12",
+      "discovered": false
+    },
+    {
+      "text": "Booger",
+      "emoji": "\ud83d\udc43",
+      "discovered": false
+    },
+    {
+      "text": "Dust",
+      "emoji": "\ud83c\udf2b\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Sand",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Rain",
+      "emoji": "\ud83c\udf27\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Snot",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Tea",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Steam",
+      "emoji": "\ud83d\udca8",
+      "discovered": false
+    },
+    {
+      "text": "Phlegm",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Fog",
+      "emoji": "\ud83c\udf2b\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Teapot",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Surfer",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Teapot",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Hurricane",
+      "emoji": "\ud83c\udf00",
+      "discovered": false
+    },
+    {
+      "text": "Lava",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Cloud",
+      "emoji": "\u2601\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Dirt",
+      "emoji": "\ud83d\udca9",
+      "discovered": false
+    },
+    {
+      "text": "Tea",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Surf",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Snot",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Steampunk",
+      "emoji": "\ud83d\udd6f\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Sweat",
+      "emoji": "\ud83d\udca6",
+      "discovered": false
+    },
+    {
+      "text": "Snot",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Surf",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Honey",
+      "emoji": "\ud83c\udf6f",
+      "discovered": false
+    },
+    {
+      "text": "Dune",
+      "emoji": "\ud83c\udf35",
+      "discovered": false
+    },
+    {
+      "text": "Flower",
+      "emoji": "\ud83c\udf38",
+      "discovered": false
+    },
+    {
+      "text": "Surf",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Bee",
+      "emoji": "\ud83d\udc1d",
+      "discovered": false
+    },
+    {
+      "text": "Volcano",
+      "emoji": "\ud83c\udf0b",
+      "discovered": false
+    },
+    {
+      "text": "Storm",
+      "emoji": "\u26c8\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Surfing",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Tea",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Surfer",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Phoenix",
+      "emoji": "\ud83d\udd25",
+      "discovered": false
+    },
+    {
+      "text": "Surprise",
+      "emoji": "\ud83d\ude32",
+      "discovered": false
+    },
+    {
+      "text": "Swamp",
+      "emoji": "\ud83d\udc0a",
+      "discovered": false
+    },
+    {
+      "text": "Cough",
+      "emoji": "\ud83e\udd27",
+      "discovered": false
+    },
+    {
+      "text": "Surfer",
+      "emoji": "\ud83c\udfc4",
+      "discovered": false
+    },
+    {
+      "text": "Tornado",
+      "emoji": "\ud83c\udf2a\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Tornado",
+      "emoji": "\ud83c\udf2a\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Dragon",
+      "emoji": "\ud83d\udc09",
+      "discovered": false
+    },
+    {
+      "text": "Cloud",
+      "emoji": "\u2601\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Beach",
+      "emoji": "\ud83c\udfd6\ufe0f",
+      "discovered": false
+    },
+    {
+      "text": "Chai",
+      "emoji": "\ud83c\udf75",
+      "discovered": false
+    },
+    {
+      "text": "Tornado",
+      "emoji": "\ud83c\udf2a\ufe0f",
+      "discovered": false
+    }
+  ],
+  "darkMode": true
+};
+
+    // Store in localStorage
+    localStorage.setItem('infinite-craft-data', JSON.stringify(elementData));
+
+    // Verification code (optional)
+    const stored = localStorage.getItem('infinite-craft-data');
+    console.log('Stored data:', JSON.parse(stored));
+    
